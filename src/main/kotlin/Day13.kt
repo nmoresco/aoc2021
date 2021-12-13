0ce89ad51f9978269cb14127710745a55187c451
Nick Moresco <nick.moresco@workday.com>
2021-12-13 08:45:11 -0800

    day 13 wip

---
+++ src/main/kotlin/Day13.kt
@@ -0,0 +1,87 @@
+class Day13 : Puzzle {
+
+    data class Fold(val coord: Int, val isVertical: Boolean)
+
+    override fun solvePartOne() {
+        val input = readInput()
+
+        var map = input.first
+
+        val width = input.first.keys.maxOf { it.first }
+        val height = input.first.keys.maxOf { it.second }
+
+        input.second.first().let { fold ->
+            map = fold(map.toMutableMap(), width, height, fold)
+        }
+
+        println(map.values.size)
+    }
+
+    override fun solvePartTwo() {
+        val input = readInput()
+
+        var map = input.first
+
+        var width = map.keys.maxOf { it.first }
+        var height = map.keys.maxOf { it.second }
+
+        input.second.forEach { fold ->
+            map = fold(map.toMutableMap(), width, height, fold)
+            width = map.keys.maxOf { it.first }
+            height = map.keys.maxOf { it.second }
+        }
+
+        for (row in 0..height) {
+            for (col in 0..width) {
+                print(map.getOrDefault(col to row, '.'))
+            }
+            println()
+        }
+    }
+
+    private fun fold(
+        map: MutableMap<Pair<Int, Int>, Char>,
+        width: Int,
+        height: Int,
+        fold: Fold
+    ): Map<Pair<Int, Int>, Char> {
+        if (fold.isVertical) {
+            for (row in fold.coord..height) {
+                for (col in 0..width) {
+                    if (map.containsKey(col to row)) {
+                        map.remove(col to row)
+                        map[col to height - row] = '#'
+                    }
+                }
+            }
+        }
+        else {
+            for (row in 0..height) {
+                for (col in fold.coord..width) {
+                    if (map.containsKey(col to row)) {
+                        map.remove(col to row)
+                        map[width - col to row] = '#'
+                    }
+                }
+            }
+        }
+
+        return map
+    }
+
+    private fun readInput(): Pair<Map<Pair<Int, Int>, Char>, List<Fold>> {
+        val map = HashMap<Pair<Int, Int>, Char>()
+        val folds = mutableListOf<Fold>()
+        readPuzzle("13-1.txt").filterNot { it.trim().isEmpty() }.forEach { line ->
+            if (line.startsWith("fold")) {
+                val split = line.split('=')
+                folds.add(Fold(split[1].toInt(), split[0].last() == 'y'))
+            } else {
+                val coords = line.split(',')
+                map[coords[0].toInt() to coords[1].toInt()] = '#'
+            }
+        }
+
+        return Pair(map, folds)
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    val puzzle: Puzzle = Day12()
+    val puzzle: Puzzle = Day13()
 
     if (args.isNotEmpty() && args[0] == "p2") {
         puzzle.solvePartTwo()
